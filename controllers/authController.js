10e5dd0044529740b91deaef8e1a6d076e9eba3f
Mia Cabanza <cabanzamia@gmail.com>
2025-11-21 20:10:21 +0800

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,73 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+exports.register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Check if user exists
+    const existingUser = await User.findOne({
+      $or: [{ email }, { username }]
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ 
+        error: 'User already exists with this email or username' 
+      });
+    }
+
+    const user = await User.create({
+      username,
+      email,
+      password
+    });
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      token
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists and password is correct
+    const user = await User.findOne({ email });
+    
+    if (user && (await user.comparePassword(password))) {
+      res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+        token: generateToken(user._id)
+      });
+    } else {
+      res.status(401).json({ error: 'Invalid credentials' });
+    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
